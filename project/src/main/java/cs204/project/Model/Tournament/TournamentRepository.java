bebf4de5e7f1fd7391c8917879e8e48c32d86add
NNNnatTTT <nathan.ng777@gmail.com>
2024-10-06 16:08:14 +0800

    removed addPlayer and removePlayer from repo, need to move to service and controller

--- project/src/main/java/cs204/project/Model/Tournament/TournamentRepository.java
+++ project/src/main/java/cs204/project/Model/Tournament/TournamentRepository.java
@@ -13,11 +13,20 @@ import org.springframework.data.repository.query.Param;
 // import org.springframework.dao.EmptyResultDataAccessException;
 
 public interface TournamentRepository extends JpaRepository<Tournament, Long>{
+    // These 2 methods should be in controller
+    // Controller has add/removePlayer, which calls on Service's getPlayerList() which then
+    // calls on findById(Tournament tournament) from repo to get the tournament
+    // It then uses tournament.getPlayerList() to get the list of players
+    // Once the list of players is returned by Service to Controller, controller modifies the list, 
+    // adding or removing the player
 
-    Long addPlayer(Player player, Tournament tournament);
+    // Long addPlayer(Player player, Tournament tournament);
+    // Long removePlayer(Player player, Tournament tournament);
+
+    
     int update(Tournament tournament);
-    @Query("SELECT * FROM tournament t inner join player p on t.region = p.region where p.id = ?1", nativeQuery = true)
-
+    @Query(value = "SELECT * FROM tournament t inner join player p on t.region = p.region where p.id = ?1", nativeQuery = true)
+    // find the player's match history based on region played
     int deleteById(Long id);
     
     @Query("SELECT COUNT(t) FROM Tournament t WHERE t.region = :region")
